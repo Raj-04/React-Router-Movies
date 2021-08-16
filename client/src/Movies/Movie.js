2c9624c9d7ae3556f30e80fa4786b97b7a3da865
Raj Patel <rajpatel@Rajs-MacBook-Pro.local>
2021-08-16 19:29:10 -0400

    Stretch goals

--- client/src/Movies/Movie.js
+++ client/src/Movies/Movie.js
@@ -2,14 +2,16 @@ import React, { useState, useEffect } from 'react';
 import { useParams, useRouteMatch } from 'react-router-dom';
 import axios from 'axios';
 
+import MovieCard from './MovieCard';
+
 export default function Movie(props) {
   const [movie, setMovie] = useState();
 
-  const { movies } = props;
+  const { movies, save } = props;
 
   const { id } = useParams();
   const { url, path, params } = useRouteMatch();
-  const item = movies[id]; // movies.find(movie => movie.id == id);
+  const item = movies[id] || movies.find(movie => movie.id === Number(id));
 
   // let id = 1;
   // Change ^^^ that line and use a hook to obtain the :id parameter from the URL
@@ -36,27 +38,6 @@ export default function Movie(props) {
     return <div>Loading movie information...</div>;
   }
 
-  const { title, director, metascore, stars } = movie;
+  return MovieCard ({ movie, save, itemID: id })
 
-  return (
-    <div className="save-wrapper">
-      <div className="movie-card">
-        <h2>{title}</h2>
-        <div className="movie-director">
-          Director: <em>{director}</em>
-        </div>
-        <div className="movie-metascore">
-          Metascore: <strong>{metascore}</strong>
-        </div>
-        <h3>Actors</h3>
-
-        {stars.map(star => (
-          <div key={star} className="movie-star">
-            {star}
-          </div>
-        ))}
-      </div>
-      <div className="save-button">Save</div>
-    </div>
-  );
 }
--- client/src/Movies/SavedList.js
+++ client/src/Movies/SavedList.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import { useHistory } from 'react-router-dom';
+import { useHistory, Link, NavLink } from 'react-router-dom';
 
 export default function SavedList(props) {
+
   const history = useHistory();
   const clickHandler = () => {
     history.push('/');
-  }
-
+  };
   return (
     <div className="saved-list">
       <h3>Saved Movies:</h3>
@@ -17,11 +17,13 @@ export default function SavedList(props) {
     </div>
   );
 }
-
 function SavedDetails(props) {
   const { title, id } = props.movie;
 
   return (
-    <span className="saved-movie">{title}</span>
-  )
-}
+    <NavLink key={id} to={`/movies/${id}`}> {/* Link and NavLink only changing when you go back to home and then click on a link again, not when you are within a movie link and you try to click other links. I don't know why, but it probably has to do with the setup, or maybe due to the fact that that specific component has already been rendered. */}
+      <span className="saved-movie">{title}</span>
+    </NavLink>
+  );
+
+} 
